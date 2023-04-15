41c7efbc78daa946d5037b4d70fffe774ceb0c1d
lebrancconvas <lebrancconvas@gmail.com>
2023-04-16 04:16:20 +0700

    ✨ feat: create search box

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-
+  let hello = '';
+  function searchHandle() {
+    console.log(`Hello ${hello}`);
+  }
 </script>
 
 <svelte:head>
@@ -15,6 +18,15 @@
       <h2>ติดตามคอนเทนต์การดีเบต และ การประชันวิสัยทัศน์ของพรรคการเมืองในหัวข้อต่างๆ ตลอดช่วงก่อนการเลือกตั้งปี 2566</h2>
     </div>
   </header>
+  <section id="search-section">
+    <div id="search-box">
+      <input type="text" placeholder="ค้นหาคำที่ต้องการ" bind:value={hello} /> 
+      <button on:click={searchHandle}>ค้นหา</button>
+    </div>
+  </section>
+  <section id="display-section">
+
+  </section>
 </main>
 
 
@@ -24,5 +36,27 @@
   }
   header {
     text-align: center;
+  }
+
+  #search-section {
+    text-align: center;
+  }
+
+  #search-box > input {
+    width: 50%;
+    height: 30px;
+    border-radius: 5px;
+    font-size: 23px;
+    padding: 5px;
+  }
+
+  #search-box > button {
+    width: 100px;
+    height: 41px;
+    border-radius: 5px;
+    background-color: #1e88e5;
+    color: white;
+    outline: none;
+    cursor: pointer;
   }
 </style>
