40a648c82435a51ada50431c5382e22c22cbf81a
lebrancconvas <lebrancconvas@gmail.com>
2023-04-16 03:57:39 +0700

    ✨ feat: update font and header

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+  <title>Debate Hub</title>
+</svelte:head>
+
+<main>
+  <header>
+    <div>
+      <h1>Debate Hub</h1>
+    </div>
+    <div>
+      <h2>ติดตามคอนเทนต์การดีเบต และ การประชันวิสัยทัศน์ของพรรคการเมืองในหัวข้อต่างๆ ตลอดช่วงก่อนการเลือกตั้งปี 2566</h2>
+    </div>
+  </header>
+</main>
+
+
+<style>
+  * {
+    font-family: 'Kanit', sans-serif;
+  }
+  header {
+    text-align: center;
+  }
+</style>
